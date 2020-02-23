4df50f017ab4fe5ce428db76a929fbba0316ee7d
Ray Greycloud <rayGreycloud@gmail.com>
2020-02-23 06:29:46 -0800

    Fix Add Post errors (#41)
    * Fix new post route and re-factor layout
    
    * Move footer out of header
    
    * Update dashboard width
    
    * Correct import statement order
    
    * Quick component cleanup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react'
-import './styles/App.css'
-import Header from './components/dashboard/header/Header'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Header from './components/dashboard/header/Header'
 import DashboardContainer from './containers/DashboardContainer'
 import PostCreatorContainer from './containers/PostCreatorContainer'
 import FullPostView from './components/FullPostView'
 import HowItWorks from './components/marketing/HowItWorks'
 import Registration from './pages/registration/Registration'
+import NavFooter from './components/dashboard/footer/NavFooter'
+
+import './styles/App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Router>
-          <Header>
+          <Header />
+          <div className="dashboard-view">
             <Switch>
               <Route exact path="/" component={DashboardContainer} />
-              <Route
-                exact
-                path="/posts/</Switch>new"
-                component={PostCreatorContainer}
-              />
+              <Route exact path="/posts/new" component={PostCreatorContainer} />
               <Route path="/posts/:postID" component={FullPostView} />
               <Route exact path="/how" component={HowItWorks} />
               <Route exact path="/what" component={DashboardContainer} />
               <Route path="/register" component={Registration} />
             </Switch>
-          </Header>
+          </div>
+
+          <NavFooter />
         </Router>
       </div>
     )
--- src/components/dashboard/header/Header.js
+++ src/components/dashboard/header/Header.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
-import '../../../styles/DashboardHeader.css'
+import React from 'react'
 import { Link } from 'react-router-dom'
+
 import LoginButton from '../../LoginButton'
-import NavFooter from '../footer/NavFooter'
+
+import '../../../styles/DashboardHeader.css'
 
 // Navigation header. Holds the menu button (TODO: define functionality w/ Adam) and user profile buttons
-export default class Header extends Component {
-  render() {
-    return (
-      <div>
-        <div className="dashboard-header">
-          <span style={{ marginLeft: '4px' }}></span>
-          <Link to="/" className="dashboard-button">
-            {/* FIXME: This is off-center. Re-work the header a bit. */}
-            <span>G'dayFam</span>
-          </Link>
-          <LoginButton />
-        </div>
-        <div className="dashboard-view">
-          {this.props.children}
-          <NavFooter />
-        </div>
-      </div>
-    )
-  }
-}
+const Header = () => (
+  <div className="dashboard-header">
+    <span className="dashboard-section"></span>
+    <Link to="/" className="dashboard-button">
+      <span>G'dayFam</span>
+    </Link>
+    <LoginButton />
+  </div>
+)
+
+export default Header
